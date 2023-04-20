3d680df646ff4cdc6e8ddf2cb5e49c75643e14cf
Sergey <kuchko.02@gmail.com>
2023-04-20 19:56:49 +0300

    Get & Post requests

--- simple_fw/main.py
+++ simple_fw/main.py
@@ -1,4 +1,6 @@
+from quopri import decodestring
 from simple_fw.renderer import render
+from simple_fw.requests import *
 
 
 class PageNotFound404:
@@ -29,6 +31,20 @@ class Framework:
             view = PageNotFound404()
 
         request = {}
+        
+        # Get request method
+        method = environ["REQUEST_METHOD"]
+        request["method"] = method
+
+        if method == "POST":
+            data = PostRequest().get_request_params(environ)
+            request["data"] = Framework.decode_value(data)
+            print(f'Нам пришёл post-запрос: {Framework.decode_value(data)}')
+        elif method == "GET":
+            request_params = GetRequests().get_request_params(environ)
+            request["request_params"] = request_params
+            print(f'Нам пришли GET-параметры:'
+                    f' {Framework.decode_value(request_params)}')
 
         for front in self.fronts_obj:
             front(request)
@@ -37,3 +53,12 @@ class Framework:
         start_response(code, [("Content-Type", "text/html")])
 
         return [body.encode("utf-8")]
+    
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
