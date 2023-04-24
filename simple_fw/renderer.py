a78cf2685b068239467371a529baa6b04012df41
Sergey <kuchko.02@gmail.com>
2023-04-24 20:29:23 +0300

    Components

--- simple_fw/renderer.py
+++ simple_fw/renderer.py
@@ -1,5 +1,4 @@
-import os
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 
 
 def render(template_name, folder="templates", **kwargs):
@@ -8,8 +7,10 @@ def render(template_name, folder="templates", **kwargs):
         template_name (_type_): Template name
         folder (str, optional): Folder of templates. Defaults to "templates".
     """
-    file_path = os.path.join(folder, template_name)
-    with open(file_path, encoding="utf-8") as f:
-        template = Template(f.read())
+    env = Environment()
+
+    env.loader = FileSystemLoader(folder)
+
+    template = env.get_template(template_name)
 
     return template.render(**kwargs)
